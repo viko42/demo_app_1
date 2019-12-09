3cc84b67fba88d8ea00522c201e9ff448cf336d2
Viko <vlancien.paris@gmail.com>
2019-12-09 01:40:54 +0100

    Updates

---
+++ precache-manifest.959764ef719b11386b507c3bc3a7f4ff.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2e9dafae4ee753ba8c3682d30e98e36",
+    "url": "/demo_app_1/index.html"
+  },
+  {
+    "revision": "3e202b661cbc9c17fe78",
+    "url": "/demo_app_1/static/css/main.96b82fb6.chunk.css"
+  },
+  {
+    "revision": "f964b0c5c82aaf84caf2",
+    "url": "/demo_app_1/static/js/2.4d5e68c4.chunk.js"
+  },
+  {
+    "revision": "3e202b661cbc9c17fe78",
+    "url": "/demo_app_1/static/js/main.979a2447.chunk.js"
+  },
+  {
+    "revision": "fcc6ba4cd48be78a037c",
+    "url": "/demo_app_1/static/js/runtime-main.bc664fb7.js"
+  },
+  {
+    "revision": "bda2074c7f4b98cacd8c639a8fe6ea5d",
+    "url": "/demo_app_1/static/media/BackgroundDecor1-fs8.bda2074c.png"
+  },
+  {
+    "revision": "d07a7a6a0acf468f66d411b65682ed07",
+    "url": "/demo_app_1/static/media/illustration-main-fs8.d07a7a6a.png"
+  },
+  {
+    "revision": "0003ae66725bec2b1ac82e09b06714e7",
+    "url": "/demo_app_1/static/media/illustration-whiteglove@2x-fs8.0003ae66.png"
+  },
+  {
+    "revision": "6cd0167c46627959daaded4800410a19",
+    "url": "/demo_app_1/static/media/story.name.6cd0167c.md"
+  },
+  {
+    "revision": "ca829de2096178ee09efb429496a7c30",
+    "url": "/demo_app_1/static/media/story.team.ca829de2.md"
+  },
+  {
+    "revision": "7a2c36e443b5fa76db23ea8209e5cf1a",
+    "url": "/demo_app_1/static/media/testimonial-picture-female@2x-fs8.7a2c36e4.png"
+  },
+  {
+    "revision": "588cd8fb57647aa0a0bf0f7e0e5e25a9",
+    "url": "/demo_app_1/static/media/testimonial-picture-male@2x-fs8.588cd8fb.png"
+  }
+]);
---
+++ static/media/story.name.6cd0167c.md
@@ -0,0 +1,6 @@
+VanGoux is pronounced Van Gogh —
+as in Vincent Van Gogh meets UX.
+
+Vincent  transformed anguish to ecstatic beauty through his art. This name is our way of honoring his profound work.
+
+Check out [Dr. Who doing something similar.](https://www.google.com)
